2f135a4d851fdb2fd9ddea1fa956872803931c84
Ion Leu - BB <ion.leu@gmail.com>
2022-04-25 11:57:31 -0400

    init: add configurations settings for rollup and ts

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import pkg from "./package.json";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: {
+      name: "@li/storegify",
+      file: pkg.browser,
+      format: "umd",
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" },
+    ],
+    plugins: [typescript({ tsconfig: "./tsconfig.json" })],
+  },
+];
